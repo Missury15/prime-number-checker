e90f063371667eb8de23c03f73ab0da24614a8fb
Missury15 <tadeas.hajek.cz@gmail.com>
2025-11-30 00:24:00 +0100

    Add files via upload

---
+++ KontrolaPrvočisel/KontrolaPrvočisel/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontrolaPrvočisel
+{
+    internal class Program
+    {
+      /*  static void Main(string[] args)
+        {
+            int cislo = int.Parse(Console.ReadLine());
+            bool jeprvocislo = true;
+
+            for (int i = 2; i < cislo; i++)
+            {
+                if (cislo % i == 0)
+                {
+                    jeprvocislo = false;
+                    //Console.WriteLine($"Číslo {cislo} je dělitelné {i} ");
+                    break;
+                }
+            }
+
+            if (jeprvocislo)
+                Console.WriteLine("Číslo je prvnočíslo");
+            else Console.WriteLine("Číslo není prvočíslo");
+
+        }*/
+
+
+        public static bool jePrvocislo(int cislo)
+        {
+            bool jeprvocislo = true;
+
+            for (int i = 2; i < cislo; i++)
+            {
+                if (cislo % i == 0)
+                {
+                    jeprvocislo = false;
+                    Console.WriteLine($"Číslo {cislo} je dělitelné {i} ");
+                    break;
+                }
+            }
+
+            if (jeprvocislo)
+                return true;
+            else return false;
+
+        }
+
+        public static void Rozklad()
+        {
+            Console.WriteLine("napište celé číslo: ");
+            int cislo = int.Parse(Console.ReadLine());
+
+            while (!jePrvocislo(cislo))
+            {
+                for (int i = 2; i < cislo; i++)
+                {
+                    if (cislo %i == 0)
+                    {
+                        Console.WriteLine($"{i}*");
+                        cislo /= 1;
+                        break;
+                    }
+                }
+            }
+            Console.Write(cislo);
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
---
+++ KontrolaPrvočisel/KontrolaPrvočisel/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("KontrolaPrvočisel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OTISKW10VM")]
+[assembly: AssemblyProduct("KontrolaPrvočisel")]
+[assembly: AssemblyCopyright("Copyright © OTISKW10VM 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("ad19804c-76e1-4e08-85b6-56cc3f673b51")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
